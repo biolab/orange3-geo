2849e6343558e9d06357efadaccc934b2d2c0ae9
Kernc <kerncece@gmail.com>
2017-07-07 12:41:49 +0200

    setup.py: bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 from setuptools import setup, find_packages
 
 
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 
 ENTRY_POINTS = {
     # Entry points that marks this package as an orange add-on. If set, addon will
