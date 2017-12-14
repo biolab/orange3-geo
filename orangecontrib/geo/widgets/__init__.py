5827d00ef2448fd7872b242d0043ce55ed30cc71
Kernc <kerncece@gmail.com>
2017-12-14 08:11:09 +0100

    Category color back to more green

--- orangecontrib/geo/widgets/__init__.py
+++ orangecontrib/geo/widgets/__init__.py
@@ -5,7 +5,7 @@ ICON = "icons/category.svg"
 
 # Background color for category background in menu
 # and widget icon background in workflow.
-BACKGROUND = "#cfe8d3"
+BACKGROUND = "#a0eaa0"
 
 # Location of widget help files.
 WIDGET_HELP_PATH = (
