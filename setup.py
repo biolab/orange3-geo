60d8583dacbd53de149b810d1addb5988ec5d4ce
robert <robert.cvitkovic@gmail.com>
2019-08-29 12:31:18 +0200

    Post release 0.2.4.post1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 from setuptools import setup, find_packages
 
 
-VERSION = "0.2.4"
+VERSION = "0.2.4.post1"
 
 ENTRY_POINTS = {
     # Entry points that marks this package as an orange add-on. If set, addon will
