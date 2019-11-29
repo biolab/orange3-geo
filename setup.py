5378fb70b9de530672b55e6401ace083918383e6
Ajda Pretnar <ajdapretnar@gmail.com>
2019-11-29 13:16:55 +0100

    Release 0.2.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 from setuptools import setup, find_packages
 
 
-VERSION = "0.2.5"
+VERSION = "0.2.6"
 
 README_FILE = path.join(path.dirname(__file__), 'README.pypi')
 LONG_DESCRIPTION = open(README_FILE).read()
