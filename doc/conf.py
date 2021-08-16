dd31a243dadd2649d89e63e023ff955c5f425578
Ajda Pretnar <ajdapretnar@gmail.com>
2021-08-16 09:36:01 +0200

    Fix building documentation

--- doc/conf.py
+++ doc/conf.py
@@ -44,9 +44,6 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_parsers = {
-    '.md': CommonMarkParser,
-}
 source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
