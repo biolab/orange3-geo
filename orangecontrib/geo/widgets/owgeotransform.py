70e3f8dec4e93ab50e300505dda0ab964d9f1dca
janezd <janez.demsar@fri.uni-lj.si>
2022-02-02 20:40:49 +0100

    - Add a checkbox to replace variables instead of appending them - Add compute_value to new variables, and use domain conversion instead of `Table.add_column` - Refactor `init_attr_values`; also, require two numeric variables - Refactor `set_data` to remove duplicated code - Use `QFormLayout` - In tests, use `model.indexOf("WGS 84")` instead of `186`, which may change in the future

--- orangecontrib/geo/widgets/owgeotransform.py
+++ orangecontrib/geo/widgets/owgeotransform.py
@@ -1,6 +1,8 @@
 from itertools import chain
 
 from AnyQt.QtCore import Qt
+from AnyQt.QtWidgets import QFormLayout
+
 from pyproj import Transformer, database
 
 from orangewidget import gui
@@ -8,10 +10,10 @@ from orangewidget.widget import Msg
 
 from Orange.widgets import settings
 from Orange.widgets.widget import OWWidget
-from Orange.widgets.utils.itemmodels import DomainModel, PyListModelTooltip
+from Orange.widgets.utils.itemmodels import DomainModel, PyListModel
 from Orange.widgets.utils.signals import Input, Output
 
-from Orange.data import ContinuousVariable, Table
+from Orange.data import ContinuousVariable, Table, Domain
 from Orange.data.util import get_unique_names
 
 from orangecontrib.geo.utils import find_lat_lon
@@ -20,6 +22,20 @@ from orangecontrib.geo.utils import find_lat_lon
 def get_projections():
     supported_proj = database.query_crs_info()
     return {i[2]: f"{i[0]}:{i[1]}" for i in supported_proj}
+
+
+class GeoTransformer:
+    def __init__(self, transformer, var_lat, var_lon, column):
+        self.transformer = transformer
+        self.var_lat, self.var_lon = var_lat, var_lon
+        self.column = column
+
+    def __call__(self, data):
+        latitude = data.get_column_view(self.var_lat)[0]
+        longitude = data.get_column_view(self.var_lon)[0]
+        coords = tuple(
+            zip(*self.transformer.itransform(zip(latitude, longitude))))
+        return coords[self.column]
 
 
 class OWGeoTransform(OWWidget):
@@ -40,6 +56,7 @@ class OWGeoTransform(OWWidget):
     attr_lon = settings.ContextSetting(None)
     from_idx = settings.Setting("Slovenia 1996 / Slovene National Grid")
     to_idx = settings.Setting("WGS 84")
+    replace_original = settings.Setting(True)
 
     want_main_area = False
     resizing_enabled = False
@@ -47,99 +64,97 @@ class OWGeoTransform(OWWidget):
     EPSG_CODES = get_projections()
 
     class Error(OWWidget.Error):
-        no_lat_lon_vars = Msg(
-                "Data has no latitude and longitude variables.")
+        no_lat_lon_vars = Msg("Data has no latitude and longitude variables.")
 
     def __init__(self):
         super().__init__()
         self.data = None
 
-        box = gui.vBox(self.controlArea, box="Coordinates:")
-
+        layout = QFormLayout()
+        gui.widgetBox(self.controlArea, box="Coordinates:", orientation=layout)
         self.variable_model = DomainModel(
             order=DomainModel.MIXED, valid_types=(ContinuousVariable,))
+        args = dict(contentsLength=100, searchable=True,
+                    model=self.variable_model, orientation=Qt.Horizontal)
+        layout.addRow("Latitude:", gui.comboBox(None, self, "attr_lat", **args))
+        layout.addRow("Longitude:", gui.comboBox(None, self, "attr_lon", **args))
+        layout.addWidget(
+            gui.checkBox(
+                None, self, "replace_original", "Replace original coordinates",
+                tooltip="If unchecked, the original coordinates are retained "
+                        "and new coordinates are added as separate variables."))
 
-        geo_args = dict(contentsLength=100, searchable=True,
-                        model=self.variable_model, orientation=Qt.Horizontal)
-
-        gui.comboBox(box, self, "attr_lat", label="Latitude:", **geo_args)
-        gui.comboBox(box, self, "attr_lon", label="Longitude:", **geo_args)
-
-        box = gui.vBox(self.controlArea, box="Transformation:")
-
-        transform_model = PyListModelTooltip()
-        transform_model.setParent(self)
-        transform_model[:] = chain(list(self.EPSG_CODES))
-        transform_model.tooltips[:] = chain(list(self.EPSG_CODES))
-
-        proj_args = dict(contentsLength=100, searchable=True,
-                         model=transform_model, orientation=Qt.Horizontal,
-                         sendSelectedValue=True)
-
-        gui.comboBox(box, self, "from_idx", label="From:", **proj_args)
-        gui.comboBox(box, self, "to_idx", label="To:", **proj_args)
+        layout = QFormLayout()
+        gui.widgetBox(self.controlArea, "Transformation:", orientation=layout)
+        args["model"] = PyListModel(self.EPSG_CODES)
+        layout.addRow("From:", gui.comboBox(None, self, "from_idx", **args))
+        layout.addRow("To:", gui.comboBox(None, self, "to_idx", **args))
 
         self.commit_button = gui.button(self.controlArea, self, "&Commit",
                                         self.apply)
 
     def init_attr_values(self):
-        self.Error.clear()
-        lat, lon = None, None
-        if self.data is not None:
+        self.variable_model.set_domain(self.data.domain if self.data else None)
+        self.Error.no_lat_lon_vars.clear()
+
+        if self.variable_model.rowCount() < 2:
+            self.Error.no_lat_lon_vars()
+            lat, lon = None, None
+            self.data = None
+        else:
             lat, lon = find_lat_lon(self.data, filter_hidden=True)
             if lat is None or lon is None:
-                # we either find both or we try all numeric
-                if self.data.domain.has_continuous_attributes(
-                        include_metas=True):
-                    vars = [var for var in self.data.domain.variables
-                            if var.is_continuous] + \
-                           [var for var in self.data.domain.metas if
-                            var.is_continuous]
-                    lat, lon = vars[0], vars[0]
-                else:
-                    self.Error.no_lat_lon_vars()
-                    self.data = None
-                    lat, lon = None, None
+                lat, lon = self.variable_model[:2]
 
-        self.variable_model.set_domain(self.data.domain if self.data else None)
         self.attr_lat, self.attr_lon = lat, lon
 
     @Inputs.data
     def set_data(self, data: Table = None):
         self.data = data
         self.closeContext()
-
-        if not data:
-            self.clear()
-            self.init_attr_values()
-            self.Outputs.data.send(None)
-            return
-
         self.init_attr_values()
         self.openContext(self.data)
         self.apply()
-
-    def clear(self):
-        self.data = None
-        self.attr_lat = self.attr_lon = None
 
     def apply(self):
         if not self.data:
             self.Outputs.data.send(None)
             return
-        transformer = Transformer.from_crs(self.EPSG_CODES[self.from_idx],
-                                           self.EPSG_CODES[self.to_idx])
-        latitude = self.data.get_column_view(self.attr_lat)[0]
-        longitude = self.data.get_column_view(self.attr_lon)[0]
-        lat, lon = zip(*transformer.itransform(zip(latitude, longitude)))
 
-        var_names = [v.name for v in self.data.domain.variables +
-                     self.data.domain.metas]
-        lat_var = ContinuousVariable(get_unique_names(var_names, "lat"))
-        lon_var = ContinuousVariable(get_unique_names(var_names, "lon"))
-        out = self.data.add_column(lat_var, lat)
-        out = out.add_column(lon_var, lon)
+        out = self.data.transform(self._transformed_domain())
         self.Outputs.data.send(out)
+
+    def _transformed_domain(self):
+        dom = self.data.domain
+        orig_coords = (self.attr_lat, self.attr_lon)
+
+        names = [var.name for var in orig_coords]
+        if not self.replace_original:
+            # If appending, use the same names, just with numbers for uniqueness
+            existing = [v.name for v in chain(dom.variables, dom.metas)]
+            names = get_unique_names(existing, names)
+
+        transformer = Transformer.from_crs(
+            self.EPSG_CODES[self.from_idx], self.EPSG_CODES[self.to_idx])
+        coords = (
+            ContinuousVariable(
+                name,
+                compute_value=GeoTransformer(transformer, *orig_coords, col))
+            for col, name in enumerate(names))
+
+        if self.replace_original:
+            tr = dict(zip(orig_coords, coords))
+
+            def r(variables):
+                return [tr.get(var, var) for var in variables]
+
+            return Domain(r(dom.attributes), r(dom.class_vars), r(dom.metas))
+
+        # Put each new variable in attributes, if it was there, else to metas
+        attrs, metas = list(dom.attributes), list(dom.metas)
+        for orig, new in zip(orig_coords, coords):
+            (attrs if orig in dom.attributes else metas).append(new)
+        return Domain(attrs, dom.class_vars, metas)
 
 
 if __name__ == "__main__":
--- orangecontrib/geo/widgets/tests/test_owgeotransform.py
+++ orangecontrib/geo/widgets/tests/test_owgeotransform.py
@@ -1,6 +1,9 @@
 import unittest
+from itertools import chain
 
-from Orange.data import Table
+import numpy as np
+
+from Orange.data import Table, Domain, ContinuousVariable, DiscreteVariable
 from Orange.widgets.tests.base import WidgetTest
 from orangecontrib.geo.widgets.owgeotransform import OWGeoTransform
 
@@ -30,7 +33,7 @@ class TestOWGeoTransform(WidgetTest):
         self.assertEqual(self.widget.attr_lat.name, "sepal length")
         self.assertEqual(self.widget.attr_lon.name, "sepal length")
 
-        # test discrete data
+        # test not enough numeric variables
         titanic = Table("titanic")
         titanic = titanic[:10]
         self.send_signal(self.widget.Inputs.data, titanic)
@@ -39,16 +42,22 @@ class TestOWGeoTransform(WidgetTest):
         self.send_signal(self.widget.Inputs.data, self.india_data)
         self.assertFalse(self.widget.Error.no_lat_lon_vars.is_shown())
 
+        short_iris = iris[:10].transform(
+            Domain([iris.domain[0], iris.domain.class_var]))
+        self.send_signal(self.widget.Inputs.data, short_iris)
+        self.assertTrue(self.widget.Error.no_lat_lon_vars.is_shown())
+
     def test_data_on_output(self):
         self.send_signal(self.widget.Inputs.data, self.india_data)
+        self.widget.replace_original = False
         self.widget.apply()
         output = self.get_output(self.widget.Outputs.data)
         self.assertGreater(len(output.domain.variables),
                            len(self.india_data.domain.variables))
 
         # change settings and commit, assure output didn't change
-        # idx 186 is WGS 84
-        self.widget.controls.from_idx.activated.emit(186)
+        combo = self.widget.controls.from_idx
+        combo.activated.emit(combo.model().indexOf("WGS 84"))
         output2 = self.get_output(self.widget.Outputs.data)
         self.assertEqual(output, output2)
 
@@ -64,6 +73,73 @@ class TestOWGeoTransform(WidgetTest):
         # pressing Commit button still emits empty signal
         self.widget.apply()
         self.assertIsNone(self.get_output(self.widget.Outputs.data))
+
+    def test_coord_position(self):
+        def names(data):
+            domain = data.domain
+            return [var.name for var in chain(domain.variables, domain.metas)]
+
+        widget = self.widget
+        widget.from_idx = "Slovenia 1996 / Slovene National Grid"
+        widget.to_idx = "WGS 84"
+
+        A = np.arange(6).reshape(3, 2)
+        B = np.arange(12).reshape(3, 4)
+        B[:, 2:] = [[384788.0, 128475.0],
+                    [388624.0, 43500.0],
+                    [388668.0, 43394.0]]
+        conv = np.array([[46.28622568426956, 13.504804692172053],
+                         [45.522518503917375, 13.574276877425241],
+                         [45.52157199667168, 13.574864024802482]])
+
+        vara = [DiscreteVariable("a1", values=tuple(str(i) for i in range(6))),
+                ContinuousVariable("a2")]
+        varb = [ContinuousVariable(n) for n in ("b1", "b2", "lat", "lon")]
+
+        # Coordinates in metas
+        data = Table.from_numpy(Domain(vara, None, varb), A, None, B)
+
+        # ... replaced
+        widget.replace_original = True
+        self.send_signal(widget.Inputs.data, data)
+        out = self.get_output(widget.Outputs.data)
+        self.assertEqual(names(data), names(out))
+        np.testing.assert_equal(A, out.X)
+        np.testing.assert_equal(B[:, :2], out.metas[:, :2])
+        np.testing.assert_almost_equal(conv, out.metas[:, 2:])
+
+        # ... appended
+        widget.controls.replace_original.click()
+        self.send_signal(widget.Inputs.data, data)
+        out = self.get_output(widget.Outputs.data)
+        self.assertEqual(names(data) + ['lat (1)', 'lon (1)'], names(out))
+        np.testing.assert_equal(A, out.X)
+        np.testing.assert_equal(B, out.metas[:, :4])
+        np.testing.assert_almost_equal(conv, out.metas[:, 4:])
+
+
+        # Coordinates in attributes
+        data = Table.from_numpy(Domain(varb, None, vara), B, None, A)
+
+        # ... replaced
+        widget.replace_original = True
+        self.send_signal(widget.Inputs.data, data)
+        out = self.get_output(widget.Outputs.data)
+        self.assertEqual(names(data), names(out))
+        np.testing.assert_equal(B[:, :2], out.X[:, :2])
+        np.testing.assert_almost_equal(conv, out.X[:, 2:])
+        np.testing.assert_equal(A, out.metas)
+
+        # ... appended
+        widget.controls.replace_original.click()
+        self.send_signal(widget.Inputs.data, data)
+        out = self.get_output(widget.Outputs.data)
+        self.assertEqual(
+            names(data)[:4] + ['lat (1)', 'lon (1)'] + names(data)[-2:],
+            names(out))
+        np.testing.assert_equal(B, out.X[:, :4])
+        np.testing.assert_almost_equal(conv, out.X[:, 4:])
+        np.testing.assert_equal(A, out.metas)
 
 
 if __name__ == "__main__":
