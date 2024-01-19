e74dbaab8d93696dfefc86a8f146f19da558eff4
Janez Demšar <janez.demsar@fri.uni-lj.si>
2024-01-19 09:46:37 +0100

    Require Orange 3.34 (#175)
    * Require Orange 3.34
    
    * Update tox.ini
    
    * Update tox.ini
    
    ---------
    
    Co-authored-by: Primož Godec <p.godec9@gmail.com>

--- orangecontrib/geo/tests/test_utils.py
+++ orangecontrib/geo/tests/test_utils.py
@@ -99,17 +99,5 @@ class TestUtils(TestCase):
         self.assertEqual(attrs_for(d1, c3, c2, c1), (c3, c3))
 
 
-class TestDeprecations(unittest.TestCase):
-    def test_remove_get_column_workaround(self):
-        """
-        When this tests start to fail:
-        1. remove this test
-        2. set minimum version of Orange to 3.34 if not set yet
-        3. remove workaround from __inint__.py
-        """
-        from datetime import datetime
-        self.assertLess(datetime.today(), datetime(2024, 1, 1))
-
-
 if __name__ == "__main__":
     unittest.main()
