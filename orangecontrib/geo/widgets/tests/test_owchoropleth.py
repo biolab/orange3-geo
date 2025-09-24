66c11414dccf4cdd9dce5593e3061431e2d0d593
janezd <janez.demsar@fri.uni-lj.si>
2025-09-24 17:08:07 +0200

    Choropleth Map: Use variable palette, when given

--- orangecontrib/geo/widgets/tests/test_owchoropleth.py
+++ orangecontrib/geo/widgets/tests/test_owchoropleth.py
@@ -1,11 +1,13 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 import numpy as np
 
 from AnyQt.QtCore import QRectF, QPointF
 
-from Orange.data import Table, Domain
+from Orange.data import Table, Domain, DiscreteVariable, ContinuousVariable
+from Orange.misc.cache import memoize_method
 from Orange.widgets.tests.base import WidgetTest, WidgetOutputsTestMixin
+from Orange.widgets.utils.colorpalettes import DefaultContinuousPalette
 from Orange.widgets.visualize.owscatterplotgraph import SymbolItemSample
 from orangecontrib.geo.widgets.owchoropleth import OWChoropleth, \
     BinningPaletteItemSample, DEFAULT_AGG_FUNC
@@ -120,6 +122,54 @@ class TestOWChoropleth(WidgetTest, WidgetOutputsTestMixin):
             self.data.X[:] = np.nan
 
         self.send_signal(self.widget.Inputs.data, self.data)
+
+    def test_get_palette(self):
+        self.widget.get_regions = memoize_method(3)(
+            lambda *_: (np.arange(26), [{}] * 26, [[]] * 26))
+        self.widget.setup_plot = Mock()
+
+        a, b = (
+            DiscreteVariable("a", values=("a", "b")),
+            DiscreteVariable("b", values=tuple("abcdefghijklm")))
+        d = Domain(
+            [a, b],
+            [],
+            [ContinuousVariable("latitude"),
+             ContinuousVariable("longitude")])
+        # 0 and 3 appear only once, 13 doesn't appear at all
+        data = Table.from_numpy(
+            d,
+            np.array([[0, 1] * 13,
+                      [1, 2, 4, 5, 6, 7, 8, 9, 8, 9, 10, 10, 12,
+                       0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],]).T,
+            None,
+            np.arange(45, 97).reshape(2, 26).T
+        )
+        self.send_signal(self.widget.Inputs.data, data)
+
+        self.widget.agg_attr = data.domain["a"]
+        self.widget.agg_func = "Mode"
+        np.testing.assert_equal(
+            self.widget.get_palette().palette,
+            a.palette.palette)
+        self.widget.agg_func = "Instance Count"
+        self.assertIs(
+            self.widget.get_palette(),
+            DefaultContinuousPalette)
+
+        self.widget.agg_attr = data.domain["b"]
+        self.widget.get_agg_data()
+        self.assertIs(
+            self.widget.get_palette(),
+            DefaultContinuousPalette)
+        self.widget.agg_func = "Mode"
+        self.widget.graph.update_colors()
+        self.widget.get_agg_data()
+        palette = self.widget.get_palette().palette
+        np.testing.assert_equal(
+            palette[:-1],
+            b.palette.palette[[1, 2, 4, 5, 6, 7, 8, 9, 10, 12]])
+        np.testing.assert_equal(palette[-1], [192, 192, 192])
 
 
 class TestOWChoroplethPlotGraph(WidgetTest):
